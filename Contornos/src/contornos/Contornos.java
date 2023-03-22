ffd4de847bd3e45d493e5dc4fe661d1ecb2c5aaa
jaimesm <jaimesm@DAW06>
2023-03-22 10:46:17 +0100

    Inicio de programa

---
+++ Contornos/src/contornos/Contornos.java
@@ -0,0 +1,19 @@
+
+package contornos;
+
+/*Se creará una calculadora con un JFrame que haga las siguientes operaciones:
+  1. Suma
+  2. Resta
+  3. Multiplicación
+  4. División
+*/
+
+public class Contornos {
+
+    public static void main(String[] args) {
+        
+        
+        
+    }
+    
+}
